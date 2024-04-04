59bf18e6d443fca2a22a8abc4820fba20c81ae74
ibran-el <163338710+ibran-el@users.noreply.github.com>
2024-04-04 15:50:28 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,11 +1,7 @@
-import cherrypy
-from main import app  # Assuming your Flask app is defined in app.py
+# File: serve.py
 
-if __name__ == '__main__':
-    cherrypy.tree.graft(app, "/")  # Mount the Flask app at the root
-    cherrypy.config.update({
-        'server.socket_host': '0.0.0.0',  # Listen on all available network interfaces
-        'server.socket_port': 8080,  # Port to listen on
-    })
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+from waitress import serve
+from main import app
+
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=8080)
