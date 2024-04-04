f41520bfe8eb850b1ca23ac3350edf3784f85423
ibran-el <163338710+ibran-el@users.noreply.github.com>
2024-04-04 12:43:40 +0300

    Add files via upload

---
+++ app_processes.py
@@ -0,0 +1,92 @@
+import docx
+import os
+from langchain.chains.question_answering import load_qa_chain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+# from langchain_community.embeddings.edenai import EdenAiEmbeddings
+from langchain_community.llms import EdenAI
+from PyPDF2 import PdfReader
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+# os.environ['EDENAI_API_KEY'] = userdata.get('EDEN_KEY')
+
+from getpass import getpass
+import os
+
+load_dotenv()
+my_secret = os.getenv('GOOGLE_API_KEY')
+
+# if "GOOGLE_API_KEY" not in os.environ:
+#     os.environ["GOOGLE_API_KEY"] = getpass("AIzaSyBMCvkIaBkOqEo_dqhXzQQQ8jye4e0FM1U")
+#     my_secret = os.environ.get('GOOGLE_API_KEY')
+
+
+class DocumentProcessor:
+    def __init__(self, path_arg):
+        self.doc_path = path_arg
+
+    # files reading functions
+    def for_pdf(self, dir_path):
+        with open(dir_path, 'rb') as pfile:
+            pdf_r = PdfReader(pfile)
+            text = ""
+            for page in range(len(pdf_r.pages)):
+                text+=pdf_r.pages[page].extract_text()
+        return text
+
+    def for_doc(self, dir_path):
+        with open(dir_path, 'r'):
+            doc_r = docx.Document(dir_path)
+            text = ""
+            for par in doc_r.paragraphs:
+                text += par.text + "\n"
+        return text
+
+    def for_text(self, dir_path):
+        with open(dir_path, 'r') as file:
+            text = file.read()
+        return text
+
+    #general document reading function
+    def readFilez(self):
+        combined_txt = ""
+        for filename in os.listdir(self.doc_path):
+            file_path = os.path.join(self.doc_path, filename)
+            if filename.endswith('.txt'):
+                combined_txt += self.for_text(file_path)
+            elif filename.endswith('.docx'):
+                combined_txt += self.for_doc(file_path)
+            elif filename.endswith('.pdf'):
+                combined_txt += self.for_pdf(file_path)
+        return combined_txt
+
+
+class ChainProcessor:
+    def __init__(self, doc_text):
+        self.text = doc_text
+
+    def CProcessing(self):
+        # split text into chunks for easy processing and memory management
+        char_txt_splitter = CharacterTextSplitter(
+            separator='\n', chunk_size=1000, chunk_overlap=200, length_function=len)
+
+        text_chunks = char_txt_splitter.split_text(self.text)
+
+        #embeddings = EdenAiEmbeddings(provider='openai')
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        docsearch = FAISS.from_texts(text_chunks,embeddings)
+        llm = llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=my_secret)
+	    #llm = EdenAI(edenai_api_key=my_secret,
+                        #    provider="openai", temperature=0.5, max_tokens=250)
+
+        chain = load_qa_chain(llm, chain_type='stuff')
+
+        return docsearch, chain
+
+    def generate_response(self, query, doc_and_chain):
+        docsearch, chain = doc_and_chain
+        docs = docsearch.similarity_search(query)
+        response = chain.run(input_documents=docs, question=query)
+        return response
---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from app_processes import DocumentProcessor, ChainProcessor
+
+
+# the running codes
+data_dir = './data/'
+doc_obj = DocumentProcessor(data_dir)
+text = doc_obj.readFilez()
+chain_obj = ChainProcessor(text)
+doc_and_chain = chain_obj.CProcessing()
+
+
+app = Flask('__name__', template_folder = './templates')
+
+messages = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        q = request.form['query']
+        q+="\. Make your response neat and easy to understand"
+        messages.append({'sender': 'user', 'message': q})
+        print(f"q is {q}")
+
+        r = chain_obj.generate_response(q, doc_and_chain)
+        messages.append({'sender': 'bot', 'message': r})
+
+        return render_template('ui_.html',msg = messages)
+
+    return render_template('ui_.html')
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
