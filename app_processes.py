0507fd78a3c0fd22b232b7a6b0a43da4b619dab5
ibran-el <163338710+ibran-el@users.noreply.github.com>
2024-04-07 00:10:38 +0300

    Update app_processes.py

--- app_processes.py
+++ app_processes.py
@@ -4,13 +4,7 @@ import getpass
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_core.runnables import RunnablePassthrough
-from langchain.memory import ConversationBufferWindowMemory
-from langchain_core.prompts import PromptTemplate
-from langchain.chains import create_history_aware_retriever, RetrievalQA
-from langchain.agents import create_vectorstore_agent, AgentExecutor, Tool, initialize_agent
-from langchain.agents.agent_toolkits import VectorStoreInfo, VectorStoreToolkit
-from langchain.tools.retriever import create_retriever_tool
+from langchain.chains.question_answering import load_qa_chain
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 
@@ -71,51 +65,15 @@ class ChainProcessor:
         # split text into chunks for easy processing and memory management
         char_txt_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
         text_chunks = char_txt_splitter.split_text(self.text)
-        
-        persist_dir = 'vdb' #creating a vector db to disk
+
         embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001") #initialize embeddins
         vector_db = FAISS.from_texts(texts=text_chunks,embedding=embeddings)
-        retriever = vector_db.as_retriever(search_type='similarity', search_kwargs={"k": 3})
         
         llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=my_secret)
         
-        memory = ConversationBufferWindowMemory(memory_key ="chat_history",k=3)
-        
-        vector_i = VectorStoreInfo(
-            name = 'vector_db',
-            description = 'Find useful information from the vector only.',
-            vectorstore = vector_db
-        )
-
-        toolkit = VectorStoreToolkit(
-            llm = llm,
-            vectorstore_info = vector_i
-        )
-        
-        tools =[ create_retriever_tool(
-            retriever = retriever,
-            name = "vector_tool",
-            description = "Use this tool to retrieve information from the vector db as context",
-            document_separator = "\n"
-        )]
-        
-        template = """Answer the questions as UniVisor, a University and Career paths guide,
-        Using the given context, be creative and empathetic.
-        {chat_history}
-        Human: {input}
-        AI: """
-        
-        prompt = PromptTemplate( input_variables=["input", "chat_history"], template = template)
-        
-        agent_store = create_vectorstore_agent(
-            llm = llm,
-            toolkit = toolkit,
-            verbose = True,
-            agent_executor_kwargs = {"memory":memory},
-            kwargs = {"prompt":prompt}
-        )
-        
-        return agent_store
+        chain = load_qa_chain(llm, chain_type = 'stuff')
+       
+        return vector_db, chain
 
 
     def generate_response(self, query, chains): 
