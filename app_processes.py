65206db9a6af50f862138a4f48428883d23e2591
ibran-el <163338710+ibran-el@users.noreply.github.com>
2024-04-11 22:04:54 +0300

    Update app_processes.py

--- app_processes.py
+++ app_processes.py
@@ -84,9 +84,6 @@ class ChainProcessor:
         db, chain = db_chain
         docs = db.similarity_search(query)
         
-        response = chain.invoke(input_documents = docs, question=query)
+        response = chain.run(input_documents = docs, question=query)
 
         return response
-    
-   
-        
